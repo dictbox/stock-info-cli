b403fafc75013a232dfd60b33dc6b08f6c9ba960
wanlei <open@wanlei.net>
2022-08-01 11:27:30 +0800

    第一版达到基本可用，下一步增加相关配置。

--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module stock-info-cli
 
 go 1.18
 
-require github.com/gosuri/uilive v0.0.4
+require (
+	github.com/gosuri/uilive v0.0.4
+	github.com/tidwall/gjson v1.14.1
+)
 
 require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/tidwall/gjson v1.14.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
--- main.go
+++ main.go
@@ -2,57 +2,88 @@ package main
 
 import (
 	"fmt"
+	"github.com/gosuri/uilive"
 	"github.com/tidwall/gjson"
-	"hash"
+	"io"
 	"io/ioutil"
 	"math"
 	"net/http"
 	"strings"
 	"sync"
+	"time"
 )
 
-type github struct {
-	Name      string `json:"name"`
-	ShortName string `json:"short_name"`
-	Icons     []struct {
-		Sizes string `json:"sizes"`
-		Src   string `json:"src"`
-		Type  string `json:"type,omitempty"`
-	} `json:"icons"`
-	PreferRelatedApplications bool `json:"prefer_related_applications"`
-	RelatedApplications       []struct {
-		Platform string `json:"platform"`
-		URL      string `json:"url"`
-		ID       string `json:"id"`
-	} `json:"related_applications"`
-}
+//type github struct {
+//	Name      string `json:"name"`
+//	ShortName string `json:"short_name"`
+//	Icons     []struct {
+//		Sizes string `json:"sizes"`
+//		Src   string `json:"src"`
+//		Type  string `json:"type,omitempty"`
+//	} `json:"icons"`
+//	PreferRelatedApplications bool `json:"prefer_related_applications"`
+//	RelatedApplications       []struct {
+//		Platform string `json:"platform"`
+//		URL      string `json:"url"`
+//		ID       string `json:"id"`
+//	} `json:"related_applications"`
+//}
 
 var wg sync.WaitGroup
 var mu sync.Mutex
-var stockInfo hash.Hash
 var stocks []string
+var result gjson.Result
 
 func main() {
-	stocks = []string{"sz002701"}
-	url := "https://api.money.126.net/data/feed/%s?callback=go"
-
-	for _, v := range stocks {
-		if strings.HasPrefix(v, "sz") {
-			url = fmt.Sprintf(url, strings.Replace(v, "sz", "1", 1))
-		}
-
-		r, e := http.Get(url)
-		if e == nil {
-			s, _ := ioutil.ReadAll(r.Body)
-			body := fmt.Sprintf("%s", s)
-			body = strings.TrimRight(strings.TrimLeft(body, "go("), ")")
-			fmt.Println(body)
-			result := gjson.Parse(body)
-
-			fmt.Println(result.Get("1002701.name"))
-
-		}
+	stocks = []string{
+		"0000001", "1399006",
+		"1000651", "0600050",
+		"0513050", "0512800",
+		"1002701", "0601318",
+		"0510050", "0512010",
+		"0512980", "0513360",
 	}
+
+	count := len(stocks)
+	lines := count / 2
+	if count%2 == 0 {
+		lines++
+	}
+
+	GetStockInfo()
+
+	writer := uilive.New()
+	// start listening for updates and render
+	writer.Start()
+	var writers []io.Writer
+	writers = append(writers, writer)
+	for j := 0; j < lines; j++ {
+		writers = append(writers, writer.Newline())
+	}
+
+	chunks := ArrayChunk(stocks, 2)
+
+	for {
+
+		for i, chunk := range chunks {
+			innerWriter := writers[i]
+			for _, v := range chunk {
+				info := result.Get(v)
+				fmt.Fprintf(innerWriter, "%7s|%8.3f|%8.3f|%8.3f|%8.2f%%\t",
+					v, info.Get("high").Float(), info.Get("low").Float(), info.Get("price").Float(),
+					info.Get("percent").Float()*100)
+			}
+
+			fmt.Fprintln(innerWriter)
+		}
+
+		time.Sleep(time.Second * 10)
+		//获取待更新的数据
+		GetStockInfo()
+	}
+
+	//
+	writer.Stop() // flush and stop rendering
 
 	//r, e := http.Get("https://github.com/manifest.json")
 	//if e != nil {
@@ -66,68 +97,36 @@ func main() {
 	//fmt.Println(gobj.Icons[0].Sizes)
 
 	//test uilive
-	//writer := uilive.New()
-	//// start listening for updates and render
-	//writer.Start()
 
 	//GetStockInfo()
-	//count := len(s)
-	//
-	//lines := count / 3
-	//if count%3 == 0 {
-	//	lines++
-	//}
-	//
-	//var writers []io.Writer
-	//writers = append(writers, writer)
-	//for j := 0; j < lines; j++ {
-	//	writers = append(writers, writer.Newline())
-	//}
-	//
-	//k := 0
-	//
-	//for {
-	//	chunks := ArrayChunk(s, 3)
-	//
-	//	for i, chunk := range chunks {
-	//		innerWriter := writers[i]
-	//		for _, v := range chunk {
-	//			fmt.Fprintf(innerWriter, "%d_%d/%3.3f/%3.3f/%3.3f%%  ", i, k, 3.005, 2.85, float64(v))
-	//		}
-	//		fmt.Fprintln(innerWriter)
-	//	}
-	//
-	//	time.Sleep(time.Second * 2)
-	//	k++
-	//	//获取待更新的数据
-	//	//GetStockInfo()
-	//}
-	//
-	//writer.Stop() // flush and stop rendering
 }
 
 func GetStockInfo() {
-	for _, v := range stocks {
-		wg.Add(1)
-		go FetchData(v)
+	url := fmt.Sprintf("https://api.money.126.net/data/feed/%s?callback=go", strings.Join(stocks, ","))
+	r, e := http.Get(url)
+	if e == nil {
+		s, _ := ioutil.ReadAll(r.Body)
+		body := fmt.Sprintf("%s", s)
+		body = strings.TrimRight(strings.TrimLeft(body, "go("), ")")
+		//fmt.Println(body)
+		result = gjson.Parse(body)
 	}
-	wg.Wait()
 }
 
-func FetchData(i string) {
-	mu.Lock()
-	//s = append(s, i)
-	mu.Unlock()
-	wg.Done()
-}
+//func FetchData(i string) {
+//	mu.Lock()
+//	//s = append(s, i)
+//	mu.Unlock()
+//	wg.Done()
+//}
 
-func ArrayChunk(s []int, size int) [][]int {
+func ArrayChunk(s []string, size int) [][]string {
 	if size < 1 {
 		panic("size: cannot be less than 1")
 	}
 	length := len(s)
 	chunks := int(math.Ceil(float64(length) / float64(size)))
-	var n [][]int
+	var n [][]string
 	for i, end := 0, 0; chunks > 0; chunks-- {
 		end = (i + 1) * size
 		if end > length {
