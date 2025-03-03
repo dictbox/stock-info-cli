a2d763ceedfaa6c25978737bb8da66c3b3b6ab86
wanlei <open@wanlei.net>
2025-03-03 09:01:01 +0800

    更新已废除的库。

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/apoorvam/goterminal"
 	"github.com/spf13/viper"
 	"github.com/tidwall/gjson"
-	"io/ioutil"
+	"io"
 	"math"
 	"net/http"
 	"os"
@@ -13,22 +13,6 @@ import (
 	"sync"
 	"time"
 )
-
-//type github struct {
-//	Name      string `json:"name"`
-//	ShortName string `json:"short_name"`
-//	Icons     []struct {
-//		Sizes string `json:"sizes"`
-//		Src   string `json:"src"`
-//		Type  string `json:"type,omitempty"`
-//	} `json:"icons"`
-//	PreferRelatedApplications bool `json:"prefer_related_applications"`
-//	RelatedApplications       []struct {
-//		Platform string `json:"platform"`
-//		URL      string `json:"url"`
-//		ID       string `json:"id"`
-//	} `json:"related_applications"`
-//}
 
 var wg sync.WaitGroup
 var mu sync.Mutex
@@ -45,10 +29,10 @@ func main() {
 	config.AddConfigPath(".")
 	config.SetConfigName("config")
 	config.SetConfigType("json")
-	config.ReadInConfig()
+	_ = config.ReadInConfig()
 
 	var configJson Config
-	config.Unmarshal(&configJson)
+	_ = config.Unmarshal(&configJson)
 	typeConverter := map[ExchangeType]string{
 		SH: "1",
 		SZ: "0",
@@ -66,7 +50,6 @@ func main() {
 	GetStatsCount()
 	GetStockInfo()
 
-	//writer := uilive.New()
 	writer := goterminal.New(os.Stdout)
 	// start listening for updates and render
 	//writer.Start()
@@ -82,24 +65,24 @@ func main() {
 
 	for {
 		// 打印涨跌家数
-		fmt.Fprintf(innerWriter, "U:%d|F:%d|D:%d|AMO:%.2f", upCount, flatCount, downCount, amountCount/100000000)
-		fmt.Fprintln(innerWriter)
+		_, _ = fmt.Fprintf(innerWriter, "U:%d|F:%d|D:%d|AMO:%.2f", upCount, flatCount, downCount, amountCount/100000000)
+		_, _ = fmt.Fprintln(innerWriter)
 
 		//打印指数
 		//innerWriter := writers[0]
 		for idx, index := range configJson.Index {
 			if idx > 0 && idx%3 == 0 {
-				fmt.Fprintln(innerWriter)
+				_, _ = fmt.Fprintln(innerWriter)
 			}
 
 			info := result[index.Code]
 			convert := math.Pow10(int(info.Get("f1").Int()))
-			fmt.Fprintf(innerWriter, "%6s|%8.3f|%8.3f|%8.3f|%6.2f%%\t",
+			_, _ = fmt.Fprintf(innerWriter, "%6s|%8.3f|%8.3f|%8.3f|%6.2f%%\t",
 				index.Code, info.Get("f15").Float()/convert, info.Get("f16").Float()/convert, info.Get("f2").Float()/convert,
 				info.Get("f3").Float()/100)
 		}
 
-		fmt.Fprintln(innerWriter)
+		_, _ = fmt.Fprintln(innerWriter)
 
 		for _, chunk := range chunks {
 			//innerWriter := writers[i+1]
@@ -120,20 +103,20 @@ func main() {
 						}
 					}
 
-					fmt.Fprintf(innerWriter, "%6s|%8.3f|%8.3f|%8.3f|%6.2f%%|%8.3f|%8.3f\t",
+					_, _ = fmt.Fprintf(innerWriter, "%6s|%8.3f|%8.3f|%8.3f|%6.2f%%|%8.3f|%8.3f\t",
 						v.Code, info.Get("f15").Float()/convert, info.Get("f16").Float()/convert, price,
 						info.Get("f3").Float()/100, buyPrice, sellPrice)
 				} else {
-					fmt.Fprintf(innerWriter, "%6s|%8.3f|%8.3f|%8.3f|%6.2f%%|%8.3f|%8.3f\t",
+					_, _ = fmt.Fprintf(innerWriter, "%6s|%8.3f|%8.3f|%8.3f|%6.2f%%|%8.3f|%8.3f\t",
 						v.Code, info.Get("f15").Float()/convert, info.Get("f16").Float()/convert, info.Get("f2").Float()/convert,
 						info.Get("f3").Float()/100, buyPrice, sellPrice)
 				}
 			}
 
-			fmt.Fprintln(innerWriter)
+			_, _ = fmt.Fprintln(innerWriter)
 		}
 
-		writer.Print()
+		_ = writer.Print()
 
 		time.Sleep(time.Second * 5)
 		//获取待更新的数据
@@ -141,22 +124,6 @@ func main() {
 		GetStockInfo()
 		innerWriter.Clear()
 	}
-
-	//
-	writer.Reset() // flush and stop rendering
-
-	//r, e := http.Get("https://github.com/manifest.json")
-	//if e != nil {
-	//	panic(e)
-	//}
-	//gobj := github{}
-	//err := json.NewDecoder(r.Body).Decode(&gobj)
-	//if err != nil {
-	//	return
-	//}
-	//fmt.Println(gobj.Icons[0].Sizes)
-
-	//test uilive
 
 	//GetStockInfo()
 }
@@ -167,7 +134,7 @@ func GetStockInfo() {
 	url := fmt.Sprintf("https://push2.eastmoney.com/api/qt/ulist/get?invt=3&pi=0&pz=30&mpi=2000&secids=%s&fields=f1,f2,f3,f12,f13,f14,f15,f16,f17,f18&po=1", strings.Join(stocks, ","))
 	r, e := http.Get(url)
 	if e == nil {
-		s, _ := ioutil.ReadAll(r.Body)
+		s, _ := io.ReadAll(r.Body)
 		body := fmt.Sprintf("%s", s)
 		body = strings.TrimRight(strings.TrimLeft(body, "go("), ")")
 		//fmt.Println(body)
@@ -191,7 +158,7 @@ func GetStatsCount() {
 	url := fmt.Sprintf("https://push2.eastmoney.com/api/qt/ulist.np/get?fields=f1,f2,f3,f4,f6,f104,f105,f106,f152&secids=1.000001,0.399001")
 	r, e := http.Get(url)
 	if e == nil {
-		s, _ := ioutil.ReadAll(r.Body)
+		s, _ := io.ReadAll(r.Body)
 		body := fmt.Sprintf("%s", s)
 		bodyResult := gjson.Parse(body)
 		bodyResult.Get("data.diff").ForEach(func(key, value gjson.Result) bool {
@@ -201,7 +168,6 @@ func GetStatsCount() {
 			amountCount += value.Get("f6").Float()
 			return true
 		})
-		// {"rc":0,"rt":11,"svr":182999890,"lt":1,"full":1,"dlmkts":"","data":{"total":2,"diff":[{"f1":2,"f2":315614,"f3":-29,"f4":-933,"f6":366318678808.3,"f104":1186,"f105":913,"f106":100,"f152":2},{"f1":2,"f2":1121979,"f3":-39,"f4":-4437,"f6":498207072951.0528,"f104":1818,"f105":864,"f106":83,"f152":2}]}}
 	}
 
 }
